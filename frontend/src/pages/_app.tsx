d605e1ee31e663a1902d97397ea3759946454525
taku-hu <hs.tm.ec.a.tt@gmail.com>
2021-07-27 22:02:16 +0900

    add: chakra-uiの導入

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import type { NextPage } from 'next/types'
 import type { AppProps } from 'next/app'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
