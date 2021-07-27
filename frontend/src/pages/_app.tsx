c5eee1e295ea4de3f19233cba434bbafdfee0722
taku-hu <hs.tm.ec.a.tt@gmail.com>
2021-07-28 02:26:55 +0900

    add: ログインフォームを作成

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 
 import type { NextPage } from 'next/types'
 import type { AppProps } from 'next/app'
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app'
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
+      <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
   )
